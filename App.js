9a00f817cb11d6f1c746c3d9dc7fbc9618e08e4a
Guilherme Barros <g.henrique.barros21@gmail.com>
2023-11-18 12:02:31 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
+
+let timer = null;
+
+let ss = 0;
+let mm = 0;
+let hh = 0;
 
 export default function App() {
+
+  const [numero, setNumero] = useState(0);
+  const [botao, setBotao] = useState('INICIAR');
+  const [ultimo, setUltimo] = useState(null);
+
+  function iniciar(){
+    if(timer !== null){
+      //vai parar o cronometro
+      clearInterval(timer);
+      timer = null;
+
+      setBotao('INICIAR'); 
+    }else {
+      //Começar a girar o cronometro
+      timer = setInterval(() => {
+        ss++;
+
+        if(ss == 60){
+          ss == 0;
+          mm++;
+        }
+
+        if(mm == 60) {
+          mm == 0;
+          hh++;
+        }
+
+        let format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+        
+
+        setNumero(format)
+
+      }, 1000);
+
+      setBotao('PAUSAR')
+    }
+  }
+
+  function limpar(){
+    if(timer !==null) {
+      //Parar o cronometro!
+      clearInterval(timer);
+      timer = null;
+    }
+
+    setUltimo(numero);
+    setNumero(0);
+
+    let ss = 0;
+    let mm = 0;
+    let hh = 0;
+    setBotao('INICIAR')
+
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+    <Image source={require('./src/crono.png')}
+    style={styles.img}
+    />
+    
+    <Text style={styles.timer}>
+      {numero}
+    </Text>
+
+    <View style={styles.btnArea}>
+    <TouchableOpacity style={styles.btn} onPress={iniciar}>
+      <Text style={styles.btnTexto}> {botao} </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.btn} onPress={limpar}>
+      <Text style={styles.btnTexto}>LIMPAR</Text>
+    </TouchableOpacity>
+
+    </View>
+
+    <View style={styles.areaUltima}>
+      <Text style={styles.textoCorrida}> 
+        {ultimo ? 'Ultimo tempo: ' + ultimo : ''}
+      </Text>
+    </View>
+
     </View>
   );
 }
@@ -16,5 +103,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00aeef'
+  },
+  img: {
+
+  },
+  timer:{
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 40,
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: 40,
+    margin: 17,
+    borderRadius: 10
+  },
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  },
+  areaUltima: {
+    marginTop: 40,
+    
+  },
+  textoCorrida:{
+    fontSize: 25,
+    color: '#fff',
+    fontStyle: 'italic'
   },
 });
